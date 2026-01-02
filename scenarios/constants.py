a729d9cfcbcaf433621afc2cd999d820c458af7e
maikleProko <maiklProko@gmail.com>
2026-01-02 07:40:02 +0300

    update

--- scenarios/constants.py
+++ scenarios/constants.py
@@ -4,7 +4,7 @@ from scenarios.masters.instances.choch_master import CHoCHMaster
 
 #FOR HISTORICAL TRADING
 realtime = False
-start_time_string='2025/12/01 00:00'
+start_time_string='2025/12/04 00:00'
 end_time_string='2025/12/10 07:00'
 is_printed_ticks = False
 balance_usdt = BalanceUSDT(7712)
