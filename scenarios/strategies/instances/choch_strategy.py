023e277c03462b23f30916a3ca768434f173cebc
maikleProko <maiklProko@gmail.com>
2026-01-02 07:39:22 +0300

    update

--- scenarios/strategies/instances/choch_strategy.py
+++ scenarios/strategies/instances/choch_strategy.py
@@ -10,6 +10,7 @@ class CHoCHStrategy(Strategy):
         self,
         history_market_parser_1m: HistoryMarketParser,
         history_market_parser_15m: HistoryMarketParser,
+        kama_indicator_30m: KamaIndicator,
         kama_indicator_15m: KamaIndicator,
         kama_indicator_1m: KamaIndicator,
         choch_indicator: CHoCHIndicator,
@@ -18,6 +19,7 @@ class CHoCHStrategy(Strategy):
         super().__init__()
         self.history_market_parser_1m=history_market_parser_1m
         self.history_market_parser_15m=history_market_parser_15m
+        self.kama_indicator_30m=kama_indicator_30m
         self.kama_indicator_15m=kama_indicator_15m
         self.kama_indicator_1m=kama_indicator_1m
         self.choch_indicator=choch_indicator
@@ -28,7 +30,8 @@ class CHoCHStrategy(Strategy):
            self.choch_indicator.choch_cross_price <= self.history_market_parser_1m.df['close'].iloc[-1] and \
            self.kama_indicator_15m.trend == "BULLISH" and \
            self.kama_indicator_15m.trend2 == "BULLISH" and \
-           self.kama_indicator_15m.trend3 == "BULLISH":
+           self.kama_indicator_15m.trend3 == "BULLISH" and \
+           self.kama_indicator_30m.trend == "BULLISH":
             self.is_accepted_by_strategy = True
         else:
             self.is_accepted_by_strategy = False
